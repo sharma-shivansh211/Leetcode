148c070574bdea931ac320aabc70c010d0c6b3c2
sharma-shivansh211 <shivansh.sharma.study@gmail.com>
2026-06-02 00:59:55 +0530

    Time: 5 ms (78.45%) | Memory: 45.8 MB (85.81%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,14 +1,24 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if( x < 0)
-            return false;
-        int n = String.valueOf(Math.abs(x)).length();
-        for(int i = n-1, j =0 ; j < i ;i--, j++){
-            int a = (x /((int)(Math.pow(10,i))))%10;
-            int b = (x /((int)(Math.pow(10,j))))%10;
-            if(a !=b )
-                return false;
+        if (x < 0) return false;
+
+        int right = 1;
+        while (x / right >= 10) {
+            right *= 10;
         }
+
+        int left = 1;
+
+        while (left < right) {
+            int a = (x / right) % 10;
+            int b = (x / left) % 10;
+
+            if (a != b) return false;
+
+            left *= 10;
+            right /= 10;
+        }
+
         return true;
     }
 }
