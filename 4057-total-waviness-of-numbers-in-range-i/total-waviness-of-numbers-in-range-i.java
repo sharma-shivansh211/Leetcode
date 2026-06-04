e716f2e70fa6dd7b2e31aa51afcf65070080742a
sharma-shivansh211 <shivansh.sharma.study@gmail.com>
2026-06-04 17:07:41 +0530

    Time: 77 ms (5.95%) | Memory: 46.6 MB (37.17%) - LeetSync

--- 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
+++ 4057-total-waviness-of-numbers-in-range-i/total-waviness-of-numbers-in-range-i.java
@@ -1,14 +1,18 @@
 class Solution {
     public int totalWaviness(int num1, int num2) {
         int w = 0;
+        if(num2 < 100) return 0;
         for(int i = num1 ; i <= num2 ; i++){
             int n = String.valueOf(Math.abs(i)).length();
-            if(n < 3&& n <100)
+            if(n < 3&& n < 100)
                 continue;
             for(int j = n-1, k = n-3; k >= 0 ; j--,k--){
-                if((i/(int)Math.pow(10,j))%10 > (i/(int)Math.pow(10,k+1))%10 && ((i/(int)Math.pow(10,k))%10 ) > (i/(int)Math.pow(10,k+1))%10)
+                int a = i/(int)Math.pow(10,j)%10;
+                int b = i/(int)Math.pow(10,k+1)%10;
+                int c = i/(int)Math.pow(10,k)%10;
+                if(a > b && c > b)
                     w++;
-                else if((i/(int)Math.pow(10,j))%10 < (i/(int)Math.pow(10,k+1))%10 && ((i/(int)Math.pow(10,k))%10 ) < (i/(int)Math.pow(10,k+1))%10)
+                else if(a < b && c < b)
                     w++;
             }
         }
