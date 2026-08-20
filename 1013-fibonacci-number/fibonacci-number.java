b5e336a6f5b4c0ccafc7afe2570a881db87a2e9c
sharma-shivansh211 <shivansh.sharma.study@gmail.com>
2026-08-20 18:30:17 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (86.11%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,7 +1,14 @@
 class Solution {
     public int fib(int n) {
+        int[] arr = new int[n+1];
+        return dpa(arr,n);
+        
+    }
+    public int dpa(int[] arr,int n){
+        if(arr[n] != 0) return arr[n];
         if(n == 1) return 1;
         if(n == 0) return 0;
-        return fib(n-1) + fib(n-2);
+        arr[n] = dpa(arr,n-1) + dpa(arr,n-2);
+        return arr[n];
     }
 }
